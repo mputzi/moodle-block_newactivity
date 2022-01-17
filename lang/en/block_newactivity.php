5036132f296b0a8f54f7050b4f9becf0f01026ab
Martin Putzlocher <m.putzlocher@stiftland-gymnasium.de>
2022-01-17 01:01:51 +0100

    rest of first files

---
+++ lang/en/block_newactivity.php
@@ -0,0 +1,32 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_recent_activity', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_newactivity
+ * @copyright 2022 Martin Putzlocher
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'New User activity';
+$string['newactivity'] = 'New User Activity';
+$string['newactivity:addinstance'] = 'Add a New User Activity Block';
+$string['newactivity:myaddinstance'] = 'Add a New User Activity Block to the Dashboard';
+$string['editingmessage'] = 'You have editing switched on.';
+$string['blocktitle'] = 'Your title for this block';
+$string['myrole'] = 'My role:';
